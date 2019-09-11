e3deb6639b0081479f4683d6d99fc977eff9937f
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 18:24:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,3 @@ katz_deli = []
 def line(katz_deli)
 puts "The line is currently empty."
 end
-def line (other_deli)
-other_deli.each_with_index.map do |index, name |
-"The line is currently: #{index}. #{name}"
-  end
-end
