b94ec2abe25714b0213d64d82433e4ce3f79bf46
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 20:06:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,4 +16,4 @@ def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, pos
+  return name, position 
