81f1f8aeb7cf780133ad963e029f5cccdff307f8
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 19:24:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,7 @@
 katz_deli = []
 def line(array)
   if array.length >= 1
-    new_array = []
-    counter = 1
-    array.each do |name|
+
       new_array.push("The line is currently: #{counter}.#{name}")
       counter += 1
     end
