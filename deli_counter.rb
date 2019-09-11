fb1f0aaeec21685a39c1abcf9962bfa591b9464d
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 18:22:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ puts "The line is currently empty."
 end
 def line (other_deli)
 other_deli.each_with_index.map do |index, name |
-"The line is currently: 1. Logan 2. Avi 3. Spencer"
+"The line is currently: #{}"
 end
 end
