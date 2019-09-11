7dd4a093992892f7e79298d333a3b1cf22c30055
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 20:03:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ if array.length == 0
   puts puts "The line is currently empty."
 else
   messgae = "The line is currently:"
-  array.each-with_index do | value, index|
+#  array.each-with_index do | value, index|
 #    message += " #{index.to_i = 1}. #{value}"
 #  end
 #  puts "#{message}"
