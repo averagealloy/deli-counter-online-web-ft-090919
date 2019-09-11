02b59fed9c510af04018bf3b7ff1753a2815d65c
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 20:04:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,5 @@
 # Write your code here.
 katz_deli = []
-#def line(array)
-#if array.length == 0
-#  puts puts "The line is currently empty."
-#else
-#  messgae = "The line is currently:"
-#  array.each-with_index do | value, index|
-#    message += " #{index.to_i = 1}. #{value}
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
