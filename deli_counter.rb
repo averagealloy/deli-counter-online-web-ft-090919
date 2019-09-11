e3481bda3794b7aebc3e75e371ee7ecf1b7823e6
Mike McTighe <m.mctighe10@gmail.com>
2019-09-11 19:24:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,5 @@
 # Write your code here.
 katz_deli = []
 def line(array)
-  if array.length >= 1
 
-      new_array.push("The line is currently: #{counter}.#{name}")
-
-    end
-    else
-puts "The line is currently empty."
-  end
 end
